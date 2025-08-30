5cf5e8978f581c3bf9d51e0da4d8d42403378114
sano84 <sano84@yahoo.com>
2025-08-30 10:34:19 -0400

    update ?

---
+++ my blog/script.js
@@ -0,0 +1,21 @@
+const videos = document.querySelectorAll(".p-6");
+
+videos.forEach((video) => {
+  video.addEventListener("mouseover", () => {
+    video.play();
+  });
+  video.addEventListener("mouseout", () => {
+    video.pause();
+  });
+});
+
+const videos2 = document.querySelectorAll(".p-10");
+
+videos2.forEach((video) => {
+  video.addEventListener("mouseover", () => {
+    video.play();
+  });
+  video.addEventListener("mouseout", () => {
+    video.pause();
+  });
+});
